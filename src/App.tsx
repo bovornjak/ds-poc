2f602b58959ccfe6eccb475447b20c908aabd61f
bcheeranorav <bovornjak.cheeranoravanich@agoda.com>
2021-10-14 16:16:38 +0700

    Design System POC

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+import { Typography as TypographyMui } from "@mui/material";
+import { Typography as TypographyKite } from '@kite-js/experimental';
+import { Typography as TypographyPlane } from '@plane-js/core';
+// import { coreDefaultTheme } from '@plane-js/theme';
+import { ThemeProvider as ThemeProviderMui } from '@mui/material/styles';
+import { createTheme } from '@mui/material/styles';
+// import { ThemeProvider } from "styled-components";
+
+function App() {
+    const muiTheme = createTheme({
+        typography: {
+            body1: {
+                fontSize: "18px",
+            }
+        },
+    });
+
+  return (
+    <div className="App">
+        <TypographyKite variant="body1">Design System POC - Kite.JS</TypographyKite>
+        <ThemeProviderMui theme={muiTheme}>
+            <TypographyMui variant="body1">Design System POC - MUI</TypographyMui>
+        </ThemeProviderMui>
+        <TypographyMui variant="body1">Design System POC - MUI</TypographyMui>
+        {/*<ThemeProvider theme={{...coreDefaultTheme}}>*/}
+        {/*    <TypographyPlane variant="body-1-short">Design System POC - Plane.JS</TypographyPlane>*/}
+        {/*    <TypographyPlane variant="title-1-short">Design System POC - Plane.JS</TypographyPlane>*/}
+        {/*</ThemeProvider>*/}
+        <TypographyPlane variant="body-1-short">Design System POC - Plane.JS</TypographyPlane>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { coreDefaultTheme } from '@plane-js/theme';
+import { ThemeProvider } from "styled-components";
+import { CssBaseline } from "@kite-js/core";
+
+const theme = {
+    ...coreDefaultTheme,
+    typography: {
+        body1: {
+            textSize: '14px',
+            weight: 'Strong',
+        },
+        "body-1-short": {
+            textSize: '16px',
+        },
+    },
+    font: {
+        ...coreDefaultTheme.font,
+        family: 'Helvetica',
+    },
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+      {/* How to replace Kite.JS theme */}
+      <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+      </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
